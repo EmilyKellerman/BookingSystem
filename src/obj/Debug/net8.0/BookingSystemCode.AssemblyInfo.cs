6bb8f1067e956567459111e83e839d65294caf6e
Emily <emilyckellerman@gmail.com>
2026-01-28 17:14:55 +0200

    Getting code

--- src/obj/Debug/net8.0/BookingSystemCode.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/BookingSystemCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystemCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31790b87562ef954f5ab312192b912381987b6f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe85f58c70a8246dedca401717775d17ee59e18")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystemCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystemCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
