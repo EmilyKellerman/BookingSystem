1a43fb0b8ca1a0ff5f21e5619ccf3b84edb1deeb
Emily <emilyckellerman@gmail.com>
2026-02-03 17:38:36 +0200

    final edits to Project.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,41 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers(); //tells ASP.NET that this application will use controllers as entry points
+builder.Services.AddSwaggerGen();
+
+//persistence file -> interface
+builder.Services.AddSingleton<ICalculationStore>(new FileCalculationStore("Data/calculations.json"));//managing domain for you
+//persistence file
+builder.Services.AddSingleton<CalculatorService>();
 
 var app = builder.Build();
+
+app.MapControllers();//calling the controllers that will be used eventually
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();//will always be the last line of the program.cs class
