9bed6b764e0b02c3372d4f722169f4fa043a050c
Emily <emilyckellerman@gmail.com>
2026-02-04 17:51:26 +0200

    Made separate controller and dto for cancelBooking

--- API/controllers/BookingController.cs
+++ API/controllers/BookingController.cs
@@ -48,25 +48,5 @@ namespace API.controllers
             
         }
 
-        [HttpDelete] //DELETE /api/bookings
-        public async Task<IActionResult> CancelBooking([FromBody] CancelBookingDto dto)
-        {
-            if(!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            else
-            {
-                var result = new BookingRequest(dto.room, dto.startTime, dto.endTime);
-
-                if (!await _manager.CancelBooking(result))
-                {
-                    return BadRequest("Invalid input");
-                }
-
-                return Ok("Successfully cancelled the booking");
-            }
-        }
-
     }
 } 
