fbd23fe1553140c72d14361a3694760b55efb6c2
Emily <emilyckellerman@gmail.com>
2026-02-07 11:42:37 +0200

    created 1 instance of each type of user

--- API/Auth/IdentitySeeder.cs
+++ API/Auth/IdentitySeeder.cs
@@ -23,11 +23,48 @@ public static class IdentitySeeder
             admin = new ApplicationUser
             {
                 UserName = "Skye",
-                Email = "Skye@Calculator.com"
+                Email = "Skye@booking.com"
             };
 
             await userManager.CreateAsync(admin, "Skye123!");
             await userManager.AddToRoleAsync(admin, "Admin");
+        }
+
+        // Create one user for each non-admin role
+        var employee = await userManager.FindByNameAsync("employee1");
+        if (employee == null)
+        {
+            employee = new ApplicationUser
+            {
+                UserName = "employee1",
+                Email = "employee@booking.com"
+            };
+            await userManager.CreateAsync(employee, "Employee123!");
+            await userManager.AddToRoleAsync(employee, "Employee");
+        }
+
+        var receptionist = await userManager.FindByNameAsync("reception1");
+        if (receptionist == null)
+        {
+            receptionist = new ApplicationUser
+            {
+                UserName = "reception1",
+                Email = "reception@booking.com"
+            };
+            await userManager.CreateAsync(receptionist, "Reception123!");
+            await userManager.AddToRoleAsync(receptionist, "Receptionist");
+        }
+
+        var facilities = await userManager.FindByNameAsync("facilities1");
+        if (facilities == null)
+        {
+            facilities = new ApplicationUser
+            {
+                UserName = "facilities1",
+                Email = "facilities@booking.com"
+            };
+            await userManager.CreateAsync(facilities, "Facilities123!");
+            await userManager.AddToRoleAsync(facilities, "Facilities Manager");
         }
     }
 }
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3746271eacdd3f22edde86186cc43d6cce92054a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63d396ba3ada6c259e50c64add6a01a3f283b31")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
